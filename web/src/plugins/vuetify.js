a4cee45bdaf0374df484389797ae055b598984fd
Zoltan Papp <zoltan.papp@webkeyapp.com>
2022-02-12 18:43:19 +0100

    try to fix vuetify icons

--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import '@mdi/font/css/materialdesignicons.css'
 import Vuetify from 'vuetify'
 import theme from './theme'
 
 Vue.use(Vuetify)
 
-const opts = { theme }
+const opts = {
+    theme,
+    icons: {
+        iconfont: 'mdi', // default - only for display purposes
+    },
+}
 
 export default new Vuetify(opts)
