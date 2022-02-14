65df632dc76605f4c19a2abb803aa4e07f810a60
Zoltan Papp <zoltan.papp@webkeyapp.com>
2022-02-14 13:45:00 +0100

    add server side authentication db

--- service/auth.go
+++ service/auth.go
@@ -1,12 +1,62 @@
 package main
 
-type MqttAuth struct{}
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strings"
+)
+
+var (
+	errInvalidCredential = errors.New("invalid credential")
+)
+
+type MqttAuth struct {
+	userStore map[string]string
+}
+
+func newMqttAuth() (*MqttAuth, error) {
+	ma := &MqttAuth{
+		make(map[string]string),
+	}
+
+	err := ma.readUserDb()
+	return ma, err
+}
 
 func (a *MqttAuth) Authenticate(user, password []byte) bool {
-	log.Printf("user: %s, %s", user, string(password))
-	return true
+	log.Infof("authenticate user: %s, %s", user, string(password))
+	pwd, ok := a.userStore[string(user)]
+	if !ok {
+		return false
+	}
+
+	return pwd == string(password)
 }
 
 func (a *MqttAuth) ACL(user []byte, topic string, write bool) bool {
 	return true
+}
+
+func (a *MqttAuth) readUserDb() error {
+	file, err := os.Open("/etc/kesmarki/users")
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		u := strings.Split(scanner.Text(), ":")
+		if len(u) != 2 {
+			return errInvalidCredential
+		}
+		a.userStore[u[0]] = u[1]
+	}
+
+	return nil
 }
