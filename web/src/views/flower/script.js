488920eb2240f0f2bdf2b1fe36ad995143bc7da5
Zoltan Papp <zoltan.papp@webkeyapp.com>
2022-12-03 14:39:17 +0100

    Remove color picker

--- web/src/views/flower/script.js
+++ web/src/views/flower/script.js
@@ -2,36 +2,46 @@ export default {
   name: 'Flower',
   mounted() {
     this.$mqtt.subscribe('kesmarki/light/flower')
-    this.defaultColor = {}
-    this.color = {
-      rgba: {
-        "r": 50,
-        "g": 0,
-        "b": 100,
-        "a": 0,
-      },
-    };
   },
   data() {
     return {
-      color: null
+      ledMode: true,
     }
   },
   watch: {
-    color() {
-      this.setLedColor()
+    ledMode(){
+      this.setMode()
     }
   },
   mqtt: {
     'kesmarki/light/flower' (data) {
       var txt = new TextDecoder().decode(data)
-      this.defaultColor = JSON.parse(txt)
-      console.log('default color: '+this.defaultColor)
+      var payload = JSON.parse(txt)
+      switch (payload.action) {
+        case 'demo':
+          this.ledMode = true
+          break
+        case 'off':
+          this.ledMode = false
+          break
+      } 
     }
   },
   methods: {
-    setLedColor() {
-      this.$mqtt.publish('kesmarki/led',  JSON.stringify(this.color.rgba), {retain: true})
+    setMode() {
+      var msg = {
+        action: ""
+      } 
+      switch (this.ledMode) {
+        case true:
+          msg.action = "demo"
+          break
+        case false:
+          msg.action = "off"
+          break
+      } 
+
+      this.$mqtt.publish('kesmarki/light/flower',  JSON.stringify(msg), {retain: true})
     }
   }
 }
